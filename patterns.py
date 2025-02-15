e39c698de31638947e818a526058041c93ff0268
Tejas N <tejasnj14@gmail.com>
2025-02-15 16:32:41 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,94 @@
+# print("*"*5) # print star 5 times
+
+# for i in range(5):
+#     print("*",end=" ") # print star 5 times
+
+# n=5
+# for i in range(n):
+#     for j in range(n):
+#         print("*",end=" ")  # print square of 5x5 dimension
+#     print()    
+
+# end=""
+# end=" " plays important role
+
+# n=5
+# for i in range(n):
+#     for j in range(i+1):  # increasing triangle pattern
+#         print("*",end="")
+#     print()    
+
+# n = 5
+# for i in range(n):
+#     for j in range(i,n): # decreasing triangle pattern
+#         print("*",end=" ")
+#     print()
+
+# n=5
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ",end=" ")
+#     for j in range(i+1):
+#         print("*",end=" ") # right sided triangle
+#     print()
+
+# n=5
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ",end=" ")
+#     for j in range(i,n):
+#         print("*",end=" ") # left sided right triangle
+#     print()
+
+# n=5
+# for i in range(n):
+#     print(" ",end=" ")
+#     for j in range(i,n):
+#         print(" ",end=" ")
+#     for j in range(i):
+#         print("*",end=" ") # hill pattern
+#     for j in range(i+1):
+#         print("*",end=" ") 
+#     print()
+
+# n=5
+# for i in range(n):
+#     print(" ",end=" ")
+#     for j in range(i+1):
+#         print(" ",end=" ")
+#     for j in range(i,n-1):
+#         print("*",end=" ") # reverse hill pattern
+#     for j in range(i,n):
+#         print("*",end=" ") 
+#     print()
+
+
+# # diamond pattern
+# n=5
+# for i in range(n-1):
+#     print(" ",end=" ")
+#     for j in range(i,n):
+#         print(" ",end=" ")
+#     for j in range(i):
+#         print("*",end=" ") 
+#     for j in range(i+1):
+#         print("*",end=" ") 
+#     print()
+# for i in range(n):
+#     print(" ",end=" ")
+#     for j in range(i+1):
+#         print(" ",end=" ")
+#     for j in range(i,n-1):
+#         print("*",end=" ") 
+#     for j in range(i,n):
+#         print("*",end=" ") 
+#     print()
+
+
+# square = [x**2 for x in range(101)]
+# print(square)
+
+# name = ["tejas","vedansh","junk"]
+# for i in range(len(name)): # for loop always contains integers
+#     print(name[i])
+
